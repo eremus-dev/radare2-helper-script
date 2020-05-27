6f077616e6df71fba0105f8a3bb6efd2be0a0dd3
sam <48877527+s0larpunk@users.noreply.github.com>
2020-05-27 15:55:02 +0800

    changed github repo

--- R2Helper.py
+++ R2Helper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.7
 """ 
-Author: isprobsbroken
-Repo: github.com/isprobsbroken
+Author: sam
+Repo: github.com/s0larpunk
  
 Class of helper functions for scripting radare2 framework.
 To hopefully stop writing the same functions again and again.
